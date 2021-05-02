e3de1a3bbbfb6985d1f0439f90c40e096f2dce95
thulka2 <thulka2@uic.edu>
2021-05-01 21:48:18 -0500

    added fully functional filter page and map updating

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let parkJSON;
-let uniqueZips;
+let uniqueZips = [];
 let map;
+
+let drawerList;
+
+let db = new Dexie("basic_database");
 
 // chip name -> json entry name
 const amenityMap = {
@@ -17,6 +21,11 @@ const amenityMap = {
     'Pool (Outdoor)': 'pool_outdo',
     'Skate Park': 'skate_park',
     'Tennis': 'tennis_cou'
+
+}
+
+// zipcode => longitude and latitude 
+let zipcodeMap = {
 
 }
 
@@ -39,7 +48,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     // open the home page by default
     document.querySelector('#home').style.display = "block";
     
-  
+
     
 
     // get advice for each page load
@@ -73,14 +82,21 @@ window.addEventListener('DOMContentLoaded', (event) => {
         mdc.textField.MDCTextField.attachTo(tf);
     })
 
+    let buttons = document.querySelectorAll('.mdc-button');
+
+    buttons.forEach( (bt)=> {
+        mdc.ripple.MDCRipple.attachTo(bt);
+    })
 
     document.querySelector('.mdc-slider').addEventListener("MDCSlider:change", () => {
         // console.log(slider.getValue());
         document.querySelector('#maxmiles p').innerHTML = `${slider.getValue()}  miles`;
     })
 
+    drawerList = mdc.list.MDCList.attachTo(document.querySelector('.mdc-drawer__content .mdc-list'));
     
-   
+    const snackbar = mdc.snackbar.MDCSnackbar.attachTo(document.querySelector('.mdc-snackbar'));
+
     // if(document.querySelector('.mdc-slider')) {
 
     // }
@@ -94,6 +110,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
                 screen.style.display = "none"; // get rid of the open screens
             })
 
+        
+        
             let screen = event.target.getAttribute("data-screen");
             if (screen == "map") {
                 updateMap();
@@ -112,7 +130,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         })
     });
 
-
+    
    
     
 
@@ -165,6 +183,20 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     })
 
+    // get location data for each zipcode 
+    fetch('https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=Chicago&rows=100&facet=state&facet=timezone&facet=dst').then( (response) => {
+        return response.json()})
+    .then ( (json) => {
+        //console.log(json);
+        json.records.forEach( (entry) => {
+            
+            if (uniqueZips.includes(parseInt(entry.fields.zip))) {
+                // if zip is in city of chicago                
+                zipcodeMap[String(entry.fields.zip)] = entry.fields.geopoint;
+            }
+        })
+    })
+
     
     // add amenity chips
     const chipSetEl = document.querySelector('.mdc-chip-set');
@@ -172,10 +204,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     const templateChip = document.querySelector('#templateChip');
     for (const [key, value] of Object.entries(amenityMap)) {
-        //console.log(`${key}: ${value}`);
         let tmp = templateChip.cloneNode(true);
         tmp.querySelector('.mdc-chip__text').innerHTML = key;
         tmp.removeAttribute('id');
+    
         chipSetEl.appendChild(tmp);
         chipSet.addChip(tmp)
 
@@ -184,6 +216,84 @@ window.addEventListener('DOMContentLoaded', (event) => {
     document.querySelector('#templateChip').style.display = 'none';
     chipSetEl.removeChild(templateChip);
 
+    
+    let filterButton = document.querySelector('#applyFilter'); 
+    filterButton.addEventListener('click', () =>  {
+        let success = true;
+
+
+        let errorMsg = document.querySelector('#errorZip');
+        let enteredZip = document.querySelector('#zipcodeTF').value
+        if (enteredZip == '') {
+            document.querySelector('.mdc-snackbar__label').innerHTML = "Zip code is required."
+            success = false;
+           
+        }
+
+        else if (zipcodeMap[parseInt(enteredZip)] == null) {
+            document.querySelector('.mdc-snackbar__label').innerHTML = "Provided zipcode is not in Chicago or currently available."
+            success = false;
+        }
+
+
+        if (success) {
+            errorMsg.style.display = 'none';
+            let selectedChips = chipSet.selectedChipIds;
+            let propertiesToCheck = [];
+            selectedChips.forEach((chip) => {
+          
+                let pos = parseInt(chip.split('-')[2]);
+                let key = chipSet.chips[pos].root.querySelector('.mdc-chip__text').innerHTML;
+                let value = amenityMap[key];
+                let properties = value.split(',');
+                properties.forEach( (p) => {
+                    propertiesToCheck.push(p.trim());
+                });
+            })
+
+            //console.log(`all properties: ${propertiesToCheck}`);
+            document.querySelectorAll(".screen").forEach((screen) => {
+                screen.style.display = "none"; // get rid of the open screens
+            })
+
+            let filteredData = [];
+            propertiesToCheck.forEach( (prop) => {
+                let f = parkJSON.filter( p => parseInt(p[prop]) > 0);
+                f.forEach ( (park) => {
+                    filteredData.push(park);
+                })
+            })
+
+            uniqueParks = [...new Set(filteredData)];
+            if(uniqueParks.length == 0) {
+                // empty set means no filters selected so use park json
+                uniqueParks = parkJSON;
+            }
+
+            // now grab only parks within given radius 
+            let miles = slider.getValue();
+            let maxDistance = miles * 1.60934;
+
+            uniqueParks = uniqueParks.filter ( p => getDistance(p, enteredZip) <= maxDistance);
+        
+
+            updateMap(uniqueParks, {lat: zipcodeMap[parseInt(enteredZip)][0], lng: zipcodeMap[parseInt(enteredZip)][1]});
+            drawerList.selectedIndex = 2; // set selection to map
+    
+           
+           
+            let targetScreen = document.querySelector("#map");
+            targetScreen.style.display = "block"; // display the map
+        } else {
+            errorMsg.style.display = 'block';
+            snackbar.open();
+        }
+
+        success = false;
+       
+
+        
+    });
 
 
 
@@ -191,15 +301,13 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     
 });
-    // topAppBar.listen('MDCTopAppBar:nav', () => {
-    //   drawer.open = !drawer.open;
-    // });
+ 
 
 
-let updateMap = () => {
-    let tmp = parkJSON;
+let updateMap = (json = parkJSON, c = {lat: 41.869, lng: -87.649}) => {
+    let tmp = json;
     map = new google.maps.Map(document.querySelector("#map-container"), {
-        center: {lat: 41.869, lng: -87.649},
+        center: c,
         zoom: 11,
     });
 
@@ -232,4 +340,35 @@ let updateMap = () => {
        
     })
     document.querySelector("#numparksdisplayed").innerHTML = `Total parks displayed: ${tmp.length}`;
+}
+
+
+// takes a park object and determines its lat, long and calls _getDistance
+let getDistance = (park, enteredZip) => {
+    let index = parseInt((park.the_geom.coordinates[0][0]).length / 4);
+    let lat = parseFloat(park.the_geom.coordinates[0][0][index][1]);
+    let long = parseFloat(park.the_geom.coordinates[0][0][index][0]);
+    //console.log(_getDistance(zipcodeMap[parseInt(enteredZip)][0], zipcodeMap[parseInt(enteredZip)][1],  lat, long))
+    return(_getDistance(zipcodeMap[parseInt(enteredZip)][0], zipcodeMap[parseInt(enteredZip)][1],  lat, long));
+}
+
+// gets distance between the origin (A zipcode geolocation) and a park 
+let _getDistance = (originLat, originLong, parkLat, parkLong) => {
+    let distance = 0;
+
+    //console.log(`${originLat}, ${originLong}, ${parkLat}, ${parkLong}`);
+    Math.radians = function(deg) {
+        return deg * (Math.PI / 180);
+    }
+
+    let lat1 = Math.radians(originLat);
+    let lng1 = Math.radians(originLong);
+    let lat2 = Math.radians(parkLat);
+    let lng2 = Math.radians(parkLong);
+
+    distance = Math.acos(Math.sin(lat1) * Math.sin(lat2) +
+				Math.cos(lat1) * Math.cos(lat2) *
+				Math.cos(lng1 - lng2)) * 6371.01; // 6371.01 is the approx radius of the earth in km
+
+    return distance; 
 }
